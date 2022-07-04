2b0919ca1dccc6c66562c81c6a312dcc6ac89aae
Okeke Juliet <chinwendujuliet457@gmail.com>
2022-07-04 15:38:47 -0700

    Task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,9 +5,9 @@
 
 
 /**
- * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ *main - prints all the numbers of base 16 in lowercase, followed by a new line
  *
- * Return: 0
+ *Return: 0
  **/
 int main(void)
 {
